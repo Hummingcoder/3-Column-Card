834a8fc89ad7515959601f7734f5607a80137b6d
Vishal D. <dwivedivishal0702@gmail.com>
2024-07-02 22:30:42 +0530

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.html"],
+  theme: {
+    extend: {
+      fontFamily: {
+        Lexend: ["Lexend Deca", "sans-serif"],
+        Big: ["Big Shoulders Display", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
